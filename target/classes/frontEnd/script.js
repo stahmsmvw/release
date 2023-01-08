c50d658c5fd8b2c83abe90a8e3aee125ab40927a
Vincent <vincentparikwestlund@gmail.com>
2023-01-09 00:41:34 +0100

    Changes from old repo

--- target/classes/frontEnd/script.js
+++ target/classes/frontEnd/script.js
@@ -7,41 +7,16 @@ var counter = 0;
  */
 
 window.onload = () => {
-    newNewerRound();
+    newRound();
 }
 
 /**
- * Most of the prep-work for the guessing-game. Ajax is used to grab @param {} movieID for much of the use seen below. 
- * @returns 
+ * <---------------------------------------------------- Web Stuff ---------------------------------------------------->
+ *
+ * Most of the prep-work for the guessing-game. Fetch is used to grab @param {} movieID for much of the use seen below. 
  */
 
-async function newRound() {
-
-    let endpoint = 'http://localhost:10123/api/movieid/game-instance';
-
-    return $.ajax({
-      method: "GET",
-      url: endpoint,
-      headers: { "Accept": "application/json" }
-    }).done(function(response) {
-        let gameIDs = [];
-        for(const obj of response) {
-            gameIDs.push(obj.movieID);
-            ids.push(obj.movieID);
-        }
-        let chosenID = assignCorrectAnswer(ids);
-        pushPosters(ids);
-        pushWebPlayer(chosenID);
-    }).catch(error => {
-        console.log(error);
-    });
-}
-
-/**
- * @todo NOT TESTED. Synchronous version of the same script above. 
- */
-
-function newNewerRound() {
+function newRound() {
     fetch('http://localhost:10123/api/movieid/game-instance')
     .then((response) => response.json())
     .then((data) => {
@@ -57,40 +32,9 @@ function newNewerRound() {
 }
 
 /**
- * 
- * @param {*} movieID Chosen MovieID to be extracted from OMDB
- */
-
-function pushWebPlayer(movieID) {
-    let title = getOMDBTitle(movieID);
-
-    /**
-     * The method above returns the correct value in console.log (inside the method) but I can't access it through console.log here. What could be wrong?
-     */
-    //console.log(title);
-    /**
-     * What do?
-     */
-}
-
-/**
- * Fetches a title from the OMDB API for further use
- * @param {} movieID ID for the movies title.
- */
-
-function getOMDBTitle(movieID) {
-    let key = "10f5a22c";
-    let endpoint = `http://omdbapi.com/?apikey=${key}&i=${movieID}`;
-    $.get(endpoint, function(data, status) {
-        let value = `${data.Title}`;
-        return value;
-    });
-}
-
-/**
  * Takes an array of 
  * @param {*} ids and assigns a random index between 0 and 3 to be the correct ID for guessing
- */
+*/
 
 function assignCorrectAnswer(ids) {
     correctCard = ids[randomNumber()]
@@ -99,7 +43,7 @@ function assignCorrectAnswer(ids) {
 
 /**
  * @returns A random number between 0 and 3 :-)
- */
+*/
 
 function randomNumber() {
     return Math.floor(Math.random() * 4);
@@ -108,7 +52,7 @@ function randomNumber() {
 /**
  * Loops through all cards and paints the front and back with a poster grabbed from the OMDB-API
  * @param {} movieIDs ID of the poster to paint
- */
+*/
 
 function pushPosters(movieIDs) {
     let cardsFront = document.getElementsByClassName("flip-card-front");
@@ -127,10 +71,10 @@ function pushPosters(movieIDs) {
 
 function checkAnswer(index) {
     let answer = ids[index];
-    let correct = isCorrect(answer);
-    inTransitionButton(correct, index);
+    let validity = isCorrect(answer);
+    inTransitionButton(validity, index);
     resetElements();
-    newNewerRound();
+    newRound();
 }
 
 function resetElements() {
@@ -146,7 +90,7 @@ function resetElements() {
  * Takes
  * @param {*} index and
  * @returns if the guess is correct or not
- */
+*/
 
 function isCorrect(answer) {
     if(answer === correctCard) {
@@ -164,44 +108,101 @@ function isCorrect(answer) {
 
 function inTransitionButton(correctAnswer, index) {
     let foo = "btn" + index;
-    let button = document.getElementById(foo);
+    let button =  document.getElementById(foo);
+    button.addEventListener("transitionend", function() {
+        button.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        button.style.transition = "background-color 0.2s ease-in-out"; // remove the transition property
+    });
+    button.style.transition = "background-color 0.2s ease-in-out";
     if (correctAnswer) {
-        button.style.backgroundColor = "rgba(98, 236, 77, 0.5)";
-        button.innerHTML = "Correct!";
-        counter++;
-    } else {
-        button.style.backgroundColor = "rgba(255, 23, 23, 0.5)";
-        button.innerHTML = "False!";
-    }
-    button.style.transition = "background-color 0.15s ease-out";
+        button.style.backgroundColor = "rgba(98, 236, 77, 0.8)";
+    } else if (!correctAnswer) {
+        button.style.backgroundColor = "rgba(255, 23, 23, 0.8)";
+    } 
 }
 
 /**
- * Transitions all buttons from whichever color they were to neutral. To be used when resetting a round.
+ * Fetches a title from the OMDB API for further use below
+ * @param {} movieID ID for the movies title.
+ */
+
+function getOMDBTitle(movieID) {
+    let key = "10f5a22c";
+    let endpoint = `http://omdbapi.com/?apikey=${key}&i=${movieID}`;
+    $.get(endpoint, function(data, status) {
+        let value = `${data.Title}`;
+        return value;
+    });
+}
+
+/**
+ * <---------------------------------------------------- Spotify Stuff --------------------------------------------------->
 */
 
-function outTransitionButton() {
-    let buttons = document.getElementsByClassName("flip-card-back-button");
-    for(const button of buttons) {
-        button.style.backgroundColor = "";
-        button.style.transition = "background-color 0.15s ease-in-out";
-        button.innerHTML = "Choose!";
+/**
+ * Loads authorization token for the player and pushes playback for the given ID
+ */
+
+function pushWebPlayer() {
+    
+}
+
+function auth() {
+
+    var client_id = 'e46654a198024b1e97a655387975bdf3';
+    var client_secret = '38f7b34e697340989b6dff7ff9421085';
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+          'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        },
+        form: {
+          grant_type: 'client_credentials'
+        },
+        json: true
+      };
+
+    fetch('https://reqbin.com/echo/post/json', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "id": 78912 })})
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
+
+    request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+    
+        // use the access token to access the Spotify Web API
+        var token = body.access_token;
+        var options = {
+        url: 'https://api.spotify.com/v1/users/jmperezperez',
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        json: true
+        };
+        request.get(options, function(error, response, body) {
+        console.log(body);
+        });
     }
+    });
 }
 
 /**
- * 
- * <----------------------------------- SPOTIFY-STUFF ----------------------------------->
- * 
+ * Loads the API for the Spotify Web Player
+ * @param {} IFrameAPI 
  */
+
 
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
     let element = document.getElementById('embed-iframe');
     let options = {
-        width: '60%',
-        height: '200',
-        left: '50%',
-        uri: 'spotify:track:6pFiKji7aJCwMIYKROgQw9'
+        width: '25%',
+        height: '150',
+        uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
       };
     let callback = (EmbedController) => {
       document.querySelectorAll('ul#episodes > li > button').forEach(
@@ -212,9 +213,5 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
         })
     };
     IFrameAPI.createController(element, options, callback);
-};
+  };
 
-function loadSong(id) {
-    let track = "spotify:track:" + id;
-    EmbedController.loadUri(episode.dataset.track);
-}
