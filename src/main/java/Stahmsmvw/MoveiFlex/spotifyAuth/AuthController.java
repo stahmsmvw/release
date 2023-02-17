6236a2e35e07a93170b93eb244b6f53d1fde014c
Vincent <vincentparikwestlund@gmail.com>
2023-02-17 15:08:33 +0100

    final stuff..

--- src/main/java/Stahmsmvw/MoveiFlex/spotifyAuth/AuthController.java
+++ src/main/java/Stahmsmvw/MoveiFlex/spotifyAuth/AuthController.java
@@ -1,142 +1,117 @@
 package Stahmsmvw.MoveiFlex.spotifyAuth;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
 
-import java.io.IOException;
-import java.net.URI;
-
-import org.apache.hc.core5.http.ParseException;
-import org.springframework.web.bind.annotation.*;
-
-import se.michaelthelin.spotify.SpotifyApi;
-import se.michaelthelin.spotify.SpotifyHttpManager;
-import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
-import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
-import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
-import se.michaelthelin.spotify.requests.data.search.SearchItemRequest;
-import se.michaelthelin.spotify.model_objects.special.SearchResult;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author Anthon Haväng
- * This class defines the API for authenticating the server to Spotify in order to search there, and
- * the ways of using Spotify's API to search for tracks.
- */
+ * @author Anthon HavÃ¤ng
+* This class defines the API for authenticating the server to
+Spotify in order to search there, and
+* the ways of using Spotify's API to search for tracks.
+*/
 @RestController
 @RequestMapping(
-          value = "/audio",
-          headers = "Accept=application/json")
+     value = "/audio",
+     headers = "Accept=application/json")
 public class AuthController {
 
-     private static final URI redirect =
-               SpotifyHttpManager.makeUri("http://localhost:10123/callback");
+     private static final String clientId = Keys.ClientId.value();
+     private static final String clientSecret = Keys.ClientSc.value();
+     private static String accessToken = "";
 
-     /**
-      * Constructs the ADT for handling using the builder pattern. Credentials from the class Keys are
-      * used.
-      */
-     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-               .setClientId(Keys.ClientId.value())
-               .setClientSecret(Keys.ClientSc.value())
-               .setRedirectUri(redirect)
-               .build();
-
-     private static final ClientCredentialsRequest clientCredentialsRequest =
-               spotifyApi.clientCredentials().build();
-
-     /**
-      * Authenticates the server to Spotifys API using provided credentials. Access token is received that
-      * lasts for an hour.
-      */
-     public static void clientCredentials_Sync() {
+     private String getAccessToken() {
+          StringBuilder response = new StringBuilder();
           try {
-               final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
-               // Set access token for further "spotifyApi" object usage
-               spotifyApi.setAccessToken(clientCredentials.getAccessToken());
-               System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+               URL authorizationEndpoint = new URL("https://accounts.spotify.com/api/token");
+               HttpURLConnection connection = (HttpURLConnection)
+               authorizationEndpoint.openConnection();
 
-          } catch (IOException | SpotifyWebApiException | ParseException e) {
-               System.out.println("Error: " + e.getMessage());
+               connection.setRequestMethod("POST");
+               connection.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes("UTF-8")));
+               connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+
+               connection.setDoOutput(true);
+               DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+
+               outputStream.writeBytes("grant_type=client_credentials");
+               outputStream.flush();
+               outputStream.close();
+
+               BufferedReader in = new BufferedReader(new
+               InputStreamReader(connection.getInputStream()));
+               String inputLine;
+
+               while ((inputLine = in.readLine()) != null) {
+               response.append(inputLine);
+               }
+               in.close();
+
+               JSONObject jsonResponse = new JSONObject(response.toString());
+               return jsonResponse.getString("access_token");
+          } catch (Exception e) {
+               System.out.println("Error while getting accesstoken: " + e.getMessage());
           }
+          return null;   
      }
 
-     /**
-      * Uses Michael Thelins implementation as a means of searching in Spotify API.
-      *
-      * @param title used as search value
-      * @return a URI to/from a specific track, usable by for example an embedded Spotify player, as JSON
-      * @author Hadi Saghir
-      */
      @GetMapping("/search/{title}")
      @ResponseBody
      public String spotifySearch(@PathVariable String title) {
-          if (spotifyApi.getAccessToken() == null) {
-               clientCredentials_Sync();
-          }
-        SearchItemRequest sir = spotifyApi
-                  .searchItem(title + " soundtrack main theme movie", "track")
-                  .build();
+          StringBuilder response = new StringBuilder();
 
+          if (accessToken.equals("")) {
+               accessToken = getAccessToken();
+               System.out.println("ACCESS TOKEN: " + accessToken);
+          }
+          String query = title + " soundtrack main theme movie";
+          query = query.replace(" ", "%20");
+          System.out.println(query);
           try {
-               final SearchResult sr = sir.execute();
-               return sr.getTracks().getItems()[0].getUri();
-          } catch (IOException | SpotifyWebApiException | ParseException e) {
-               e.printStackTrace();
-               return null;
+               URL searchEndpoint = new URL("https://api.spotify.com/v1/search?q=" + query + "&type=track&limit=1");
+               System.out.println(searchEndpoint);
+               HttpURLConnection connection = (HttpURLConnection)
+               searchEndpoint.openConnection();
+
+               connection.setRequestProperty("Accept", "application/json");
+               connection.setRequestProperty("Content-Type", "application/json");
+               connection.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+               connection.setRequestMethod("GET");
+
+               BufferedReader in = new BufferedReader(new
+               InputStreamReader(connection.getInputStream()));
+               String inputLine;
+
+               while ((inputLine = in.readLine()) != null) {
+               response.append(inputLine);
+               }
+               in.close();
+
+
+               //System.out.println("RESPONSE: " + response.toString());
+               JSONObject jsonResponse = new JSONObject(response.toString());
+               JSONObject tracks = jsonResponse.getJSONObject("tracks");
+               JSONArray items = tracks.getJSONArray("items");
+               JSONObject firstItem = items.getJSONObject(0);
+               String uri = firstItem.getString("uri");
+               //System.out.println("PRINT URI: " + uri);
+               return uri;
+          } catch (Exception e) {
+               System.out.println("Error while searching tracks:" + e.getMessage());
+               //System.out.println("RESPONSE: " + response.toString());
+               return "ERROR 404";
           }
      }
+
 }
-
-
-//om skrivning av anrop
-/**
- * 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import java.io.IOException;
-import org.json.JSONObject;
-
-public class SpotifyAuth {
-    private static final String clientId = "YOUR_CLIENT_ID";
-    private static final String clientSecret = "YOUR_CLIENT_SECRET";
-    private static String accessToken;
-
-
-     public static void clientCredentials_Sync() {
-          OkHttpClient client = new OkHttpClient();
-
-          Request request = new Request.Builder()
-                    .url("https://accounts.spotify.com/api/token")
-                    .post(RequestBody.create(MediaType.get("application/x-www-form-urlencoded"), "grant_type=client_credentials"))
-                    .addHeader("Authorization", "Basic " + Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes()))
-                    .build();
-
-          try (Response response = client.newCall(request).execute()) {
-               String body = response.body().string();
-               JSONObject json = new JSONObject(body);
-               accessToken = json.getString("access_token");
-          }
-     }
-
-
-     @GetMapping("/search/{title}")
-     @ResponseBody
-     public String spotifySearch(@PathVariable String title) {
-          if (spotifyApi.getAccessToken() == null) {
-               clientCredentials_Sync();
-          }
-          track_name = title + " soundtrack main theme movie";
-          Request request = new Request.Builder()
-          .url("https://api.spotify.com/v1/search?q=track_name&type=track")
-          .get()
-          .addHeader("Authorization", "Bearer " + accessToken)
-          .build();
-
-          try {
-               final SearchResult sr = result.execute();
-               return sr.getTracks().getItems()[0].getUri();
-          } catch (IOException | SpotifyWebApiException | ParseException e) {
-               e.printStackTrace();
-               return null;
-          }
-
-     }
- */
